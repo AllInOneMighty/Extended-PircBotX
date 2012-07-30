adc29be01112896e6d1945e1388f0604cc9bb9ba
Emmanuel Cron <emmanuel.cron@gmail.com>
2012-07-30 23:47:19 +0200

    Documented the AutoRejoin module

--- modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/AutoRejoinPircModule.java
+++ modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/AutoRejoinPircModule.java
@@ -4,15 +4,20 @@ import org.jibble.pircbot.ExtendedPircBot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Makes the bot try to rejoin any channel when it is kicked for any reason.
+ * 
+ * @author Emmanuel Cron
+ */
 public class AutoRejoinPircModule extends AbstractPircModule {
 	private static final Logger LOGGER = LoggerFactory.getLogger(AutoRejoinPircModule.class);
 
 	@Override
 	public void onKick(ExtendedPircBot bot, String channel, String kickerNick, String kickerLogin,
-			String kickerHostname,
-			String recipientNick, String reason) {
+			String kickerHostname, String recipientNick, String reason) {
 		if (recipientNick.equals(bot.getNick())) {
-			LOGGER.info("I have been kicked from {} by {}, rejoining channel", channel, kickerNick);
+			LOGGER.info("I have been kicked from {} by {}, rejoining channel (reason: {})", new Object[] {channel,
+					kickerNick, reason});
 			bot.joinChannel(channel);
 		}
 	}
