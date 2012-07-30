17e12e4a3a4b119dac2feee80a14f22b94b2fdcb
Emmanuel Cron <emmanuel.cron@gmail.com>
2012-07-30 23:57:40 +0200

    Documented Quit module

--- modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/QuitPircModule.java
+++ modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/QuitPircModule.java
@@ -5,6 +5,12 @@ import org.jibble.pircbot.ExtendedPircBot;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Makes the bot quit when an admin sends a custom message in private chat to
+ * the bot. The quit message of the bot can also be customized.
+ * 
+ * @author Emmanuel Cron
+ */
 public class QuitPircModule extends AbstractPircModule implements PrivatePircModule {
 	
 	private static final Logger LOGGER = LoggerFactory.getLogger(QuitPircModule.class);
@@ -13,6 +19,15 @@ public class QuitPircModule extends AbstractPircModule implements PrivatePircMod
 
 	private String quitMessage;
 
+	/**
+	 * Creates a new quit module.
+	 * 
+	 * @param trigger the message that an admin needs to send to the bot to make
+	 *        it quit
+	 * @param quitMessage the message displayed to all users when the bot quits;
+	 *        this message can be <tt>null</tt> or empty if you do not want a
+	 *        quit message
+	 */
 	public QuitPircModule(String trigger, String quitMessage) {
 		this.trigger = trigger;
 		this.quitMessage = quitMessage;
