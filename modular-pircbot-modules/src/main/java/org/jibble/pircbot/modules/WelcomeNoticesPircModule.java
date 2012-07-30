a39554e2e44aed558140f5c72fd71a81ce2b546d
Emmanuel Cron <emmanuel.cron@gmail.com>
2012-07-31 00:07:25 +0200

    - Renamed OnJoin module to WelcomeNotices module - Documented this module

---
+++ modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/WelcomeNoticesPircModule.java
@@ -0,0 +1,57 @@
+package org.jibble.pircbot.modules;
+
+import java.util.List;
+
+import org.jibble.pircbot.ExtendedPircBot;
+
+/**
+ * Sends notices to any user that connects to the channel. In the notices, you
+ * may use the following codes that will automatically be replaced by the bot:
+ * <dl>
+ * <dt>{botname}</dt>
+ * <dd>Current name of the bot</dd>
+ * <dt>{channel}</dt>
+ * <dd>Channel that the user has just joined</dd>
+ * <dt>{helptrigger}</dt>
+ * <dd>Command to send to the bot to display the help. Note that you may not use
+ * this code if you do not want your users to know how to display the help.</dd>
+ * </dl>
+ * 
+ * @author Emmanuel Cron
+ */
+public class WelcomeNoticesPircModule extends AbstractPircModule {
+	private List<String> welcomeNotices;
+	
+	private String helpTrigger;
+
+	/**
+	 * Creates a new welcome module.
+	 * 
+	 * @param welcomeNotices the messages to send to a user that connects to a
+	 *        channel where the bot is connected; special codes used in these
+	 *        messages will be replaced (see class description)
+	 * @param helpTrigger the command that needs to be sent to the bot to
+	 *        display the help
+	 */
+	public WelcomeNoticesPircModule(List<String> welcomeNotices, String helpTrigger) {
+		this.welcomeNotices = welcomeNotices;
+		this.helpTrigger = helpTrigger;
+	}
+
+	@Override
+	public void onJoin(ExtendedPircBot bot, String channel, String sender, String login, String hostname) {
+		if (sender.equals(bot.getNick())) {
+			// Don't react to own joins
+			return;
+		}
+
+		if (welcomeNotices != null) {
+			for (String welcomeNotice : welcomeNotices) {
+				welcomeNotice = welcomeNotice.replace("{botname}", bot.getNick());
+				welcomeNotice = welcomeNotice.replace("{channel}", channel);
+				welcomeNotice = welcomeNotice.replace("{helptrigger}", helpTrigger);
+				bot.sendNotice(sender, welcomeNotice);
+			}
+		}
+	}
+}
