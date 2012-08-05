7b92718c1aa34a4a4f7c763dc05fc2c462a0939e
Emmanuel Cron <emmanuel.cron@gmail.com>
2012-08-05 21:04:40 +0200

    Added documentation to Facts module

--- modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/FactsPircModule.java
+++ modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/FactsPircModule.java
@@ -6,6 +6,21 @@ import java.util.Random;
 import org.jibble.pircbot.ExtendedPircBot;
 import org.jibble.pircbot.modules.facts.FactsReader;
 
+/**
+ * Displays random facts on user request. This module loads a list of facts on
+ * startup and then randomly chooses one of them and displays it on specific
+ * triggers.
+ * <p>
+ * On instantiation, the developer needs to provide an implementation of
+ * {@link FactsReader} in order to load the facts into memory. You should check
+ * which readers are available in the
+ * <tt>org.jibble.pircbot.modules.facts.*</tt> package before developing your
+ * own.
+ * <p>
+ * This module can only be triggered on a public chat.
+ * 
+ * @author Emmanuel Cron
+ */
 public class FactsPircModule extends AbstractPircModule implements PublicPircModule {
 	private static final Random RANDOM = new Random();
 
@@ -17,6 +32,14 @@ public class FactsPircModule extends AbstractPircModule implements PublicPircMod
 
 	private List<List<String>> facts;
 
+	/**
+	 * Creates a new facts module.
+	 * 
+	 * @param trigger the word to say in a public chat to trigger the display of
+	 *        a fact; note that this word must be prefixed by "<tt>!</tt>"
+	 * @param factsReader a facts reader that will be used to load the facts
+	 *        into memory at startup
+	 */
 	public FactsPircModule(String trigger, FactsReader factsReader) {
 		this.trigger = trigger;
 		this.factsReader = factsReader;
