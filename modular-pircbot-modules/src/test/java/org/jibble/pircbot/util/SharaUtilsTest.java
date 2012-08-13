58aa79619aad70ceeba8635c2cbc958615693299
Emmanuel Cron <emmanuel.cron@gmail.com>
2012-08-13 11:40:03 +0200

    - Added log4j.properties for tests - Now creating tests cache folder and using it

--- modular-pircbot-modules/src/test/java/org/jibble/pircbot/util/SharaUtilsTest.java
+++ modular-pircbot-modules/src/test/java/org/jibble/pircbot/util/SharaUtilsTest.java
@@ -1,16 +1,25 @@
 package org.jibble.pircbot.util;
 
+import java.io.File;
 import java.io.IOException;
 
 import junit.framework.Assert;
 
 import org.jibble.pircbot.util.urlshorteners.SharasURLShortener;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class SharaUtilsTest {
+	private static final String CACHE_FOLDER = "target/cache";
+
+	@BeforeClass
+	public static void createCacheFolder() {
+		new File(CACHE_FOLDER).mkdir();
+	}
+	
 	@Test
 	public void testSharasify() throws IOException {
-		SharasURLShortener sharas = new SharasURLShortener("sharascache");
+		SharasURLShortener sharas = new SharasURLShortener(CACHE_FOLDER);
 		String shortenedURL = sharas.shortenURL("http://www.judgehype.com");
 		Assert.assertEquals("http://shar.as/Fg1e3", shortenedURL);
 	}
