4b44f9d8b2ddfbe244ef9e55829e0f4a2108d1d5
Emmanuel Cron <emmanuel.cron@gmail.com>
2013-12-06 01:18:22 -0800

    Can now join several channels with the same module

--- modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/JoinOnConnectPircModule.java
+++ modular-pircbot-modules/src/main/java/org/jibble/pircbot/modules/JoinOnConnectPircModule.java
@@ -1,26 +1,37 @@
 package org.jibble.pircbot.modules;
 
+import java.util.List;
+
 import org.jibble.pircbot.ExtendedPircBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.common.collect.ImmutableList;
 
 /**
- * Makes the bot join a channel after it succesfully connects to the server.
- *
+ * Makes the bot join one or several channels after it successfully connects to the server.
+ * 
  * @author Emmanuel Cron
  */
 public class JoinOnConnectPircModule extends AbstractPircModule {
-  private String channel;
-  
+  private static final Logger LOGGER = LoggerFactory.getLogger(JoinOnConnectPircModule.class);
+
+  private List<String> channels;
+
   /**
    * Creates a new join on connect module.
    * 
-   * @param channel the channel to join when the bot is connected
+   * @param channels the channels to join when the bot is connected
    */
-  public JoinOnServerResponsePircModule(String channel) {
-    this.channel = channel;
+  public JoinOnConnectPircModule(List<String> channels) {
+    this.channels = ImmutableList.copyOf(channels);
   }
 
   @Override
   public void onConnect(ExtendedPircBot bot) {
-    bot.joinChannel(channel);
+    for (String channel : channels) {
+      LOGGER.info("Joining channel %s", channel);
+      bot.joinChannel(channel);
+    }
   }
 }
