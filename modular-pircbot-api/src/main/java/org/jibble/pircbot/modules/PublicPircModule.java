cd78192491a154692dda4fab90599723272cc868
Emmanuel Cron <emmanuel.cron@gmail.com>
2013-12-09 22:07:56 -0800

    Public modules can have a null description

--- modular-pircbot-api/src/main/java/org/jibble/pircbot/modules/PublicPircModule.java
+++ modular-pircbot-api/src/main/java/org/jibble/pircbot/modules/PublicPircModule.java
@@ -22,7 +22,7 @@ public interface PublicPircModule {
   /**
    * Returns a single line of text explaining what this module does.
    * 
-   * @return the description of this module
+   * @return the description of this module or {@code null} if no description is available
    */
   String getHelpText();
 
