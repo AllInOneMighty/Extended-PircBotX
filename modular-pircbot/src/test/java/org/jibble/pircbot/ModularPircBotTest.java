a11263087c8ba2a60b9e532d112ec281652f6f36
Emmanuel Cron <emmanuel.cron@gmail.com>
2013-12-09 22:42:30 -0800

    Slightly better tests

--- modular-pircbot/src/test/java/org/jibble/pircbot/ModularPircBotTest.java
+++ modular-pircbot/src/test/java/org/jibble/pircbot/ModularPircBotTest.java
@@ -1,21 +1,41 @@
 package org.jibble.pircbot;
 
-import java.util.List;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 
-import junit.framework.Assert;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.jibble.pircbot.modules.HelpPircModule;
 import org.junit.Test;
 
 public class ModularPircBotTest {
-	@Test
-	public void test() {
-		ModularPircBot bot = new ModularPircBot(null, null, "Test");
-		bot.addModule(new HelpPircModule("aide", "This is a test help message"));
-		
-		List<String> help = bot.buildHelp("TestUser", false);
-		// Intro is sent to channel (not accessible here)
-		Assert.assertEquals(1, help.size());
-		Assert.assertEquals("!aide", help.get(0));
-	}
+  @Test
+  public void buildHelp() {
+    List<Integer> ports = new ArrayList<Integer>();
+    ports.add(1);
+
+    ModularPircBot bot = new ModularPircBot("some host", ports, "Test");
+    bot.addModule(new HelpPircModule("aide", "This is a test help message"));
+
+    List<String> help = bot.buildHelp("TestUser", false);
+    // Intro is sent to channel (not accessible here)
+    assertEquals(1, help.size());
+    assertEquals("!aide", help.get(0));
+  }
+
+  @Test
+  public void addModule_twoHelpModules() {
+    List<Integer> ports = new ArrayList<Integer>();
+    ports.add(1);
+
+    ModularPircBot bot = new ModularPircBot("some host", ports, "Test");
+    bot.addModule(new HelpPircModule("aide", "This is a test help message"));
+    try {
+      bot.addModule(new HelpPircModule("aide", "This is a test help message"));
+      fail("Should have failed");
+    } catch (IllegalStateException ise) {
+      // expected
+    }
+  }
 }
